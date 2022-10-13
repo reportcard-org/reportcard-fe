e0ad74b5dda9e00ceb9a68cd375d188bd7bf19d8
Eddie Rodriguez <edwardjrodriguez17@gmail.com>
2022-10-13 12:08:09 -0600

    Testing again

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<Ap />);
-  const linkElement = screen.getByText(/lea react/i);
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
 
