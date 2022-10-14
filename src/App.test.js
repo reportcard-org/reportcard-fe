2e6099cdf1929e7a10bf0843a1886e5633fb6824
Eddie Rodriguez <edwardjrodriguez17@gmail.com>
2022-10-14 07:50:12 -0600

    hook up cypress

--- src/App.test.js
+++ src/App.test.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// import React from 'react';
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
 
-test('renders the word Edie', () => {
-  render(<App />);
-  expect(screen.getByText(/Edit/i)).toBeInTheDocument();
-});
+// test('renders the word Edit', () => {
+//   render(<App />);
+//   expect(screen.getByText(/Edit/i)).toBeInTheDocument();
+// });
 
-test('renders learn react link', () => {
-  render(<App />);
-  expect(screen.getByText(/Hello world/i)).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   render(<App />);
+//   expect(screen.getByText(/Hello world/i)).toBeInTheDocument();
+// });
 
 
 
