d240bd381a913d48c0f988fdd26d3c8a3dbc4b81
Patrick Becker <patrickgbecker@gmail.com>
2022-10-15 16:32:27 -0500

    Add example response prop rendering

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import DistrictInfoPage from '../DistrictInfoPage/DistrictInfoPage';
 import SearchPage from '../SearchPage/SearchPage';
@@ -6,12 +6,29 @@ import UserLoginPage from '../UserLoginPage/UserLoginPage';
 import FavoriteDistrictsPage from '../FavoriteDistrictsPage/FavoriteDistrictsPage';
 
 const App = () => {
+
+  const [districtData, setDistrictData] = useState(
+    {
+    data: {
+      id: 1,
+      type: 'school district',
+      attributes: [
+        {student_teacher_ratio: .05},
+        {per_student_expenditure: 2000},
+        {teacher_salary_info: 60000},
+        {student_population_size: 50000},
+        {number_of_schools_in_district: 138}
+      ]
+    }
+  });
+  const [userData, setUserData] = useState('');
+
   return (
     <div className="App">
-     {/* <DistrictInfoPage /> */}
-     {/* <SearchPage /> */}
-     {/* <UserLoginPage /> */}
-     {/* <FavoriteDistrictsPage /> */}
+      <UserLoginPage /> 
+      <SearchPage /> 
+      <DistrictInfoPage districtData={districtData} /> 
+      <FavoriteDistrictsPage /> 
     </div>
   );
 }
--- src/components/DistrictInfoPage/DistrictInfoPage.js
+++ src/components/DistrictInfoPage/DistrictInfoPage.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import ReportCard from '../ReportCard/ReportCard'
 import './DistrictInfoPage.css';
 
-const DistrictInfoPage = () => {
+const DistrictInfoPage = ( {districtData} ) => {
+
+    const newReportCard = districtData.data.attributes.map(attribute => {
+        return (
+            <ReportCard 
+                studentTeacherRatio = {attribute.student_teacher_ratio}
+                perStudentExpenditure={attribute.per_student_expenditure}
+                teacherSalaryInfo={attribute.teacher_salary_info}
+                studentPopulationSize={attribute.student_population_size}
+                numberOfSchoolsInDistrict={attribute.number_of_schools_in_district}
+            />
+        )
+    })
+    
     return (
         <div className='district-info-container'>
-            DistrictInfoPage WILL EXIST HERE
+            {newReportCard}
         </div>
     )
 }
