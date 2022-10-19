88ddaae77c4bd6338b76888c12bc5ff767b80dd0
Maia Cochran <maia.cochran1@gmail.com>
2022-10-18 22:11:44 -0700

    Add nav styling

--- src/components/ NavBar/NavBar.js
+++ src/components/ NavBar/NavBar.js
@@ -5,8 +5,8 @@ import {  useNavigate } from 'react-router-dom';
 
 const NavBar = () => {
     const navigate = useNavigate()
-
-    // a note about the buttons.  The buttons for routing home, favorites, etc will need to be conditionally rendered by the path of the url.  Example: when on the favorites page we dont want a button that routes us to the favorites page since we are already on it
+    // a note about the buttons.  The buttons for routing home, favorites, etc will need to be conditionally rendered by the path of the url.  
+    // Example: when on the favorites page we dont want a button that routes us to the favorites page since we are already on it
 
     return (
         <div className='nav-bar'>
