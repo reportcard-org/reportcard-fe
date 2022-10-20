c9b83cadb19273a870d9d71f62ead875b85d99c6
Eddie Rodriguez <edwardjrodriguez17@gmail.com>
2022-10-20 07:53:04 -0600

    Add tests to user search spec file

--- cypress/e2e/user-login-spec.cy.js
+++ cypress/e2e/user-login-spec.cy.js
@@ -4,15 +4,11 @@ const postStub = {
   "state": "Colorado"
 }
 
-
 describe('ReportCard login page', () => {
 
   beforeEach(() => {
     cy.visit('http://localhost:3000/')
-
     //gql stub??  
-
-    
   })
   
   it('loads the main page', () => {
@@ -25,7 +21,7 @@ describe('ReportCard login page', () => {
     
   });
   
-  it('can continue as guest without logging in', () => {
+  it('user can continue as guest without logging in', () => {
     cy.get('.nav-bar').contains('ReportCard')
     .location('pathname').should('eq', '/')
     .get('.logo').contains('✅')
@@ -36,25 +32,7 @@ describe('ReportCard login page', () => {
     .location('pathname').should('eq', '/home')
   });
   
-  it('can continue as guest and search for a district if user inputs a city, street, and state', () => {
-    cy.get('.nav-bar').contains('ReportCard')
-    .location('pathname').should('eq', '/')
-    .get('.logo').contains('✅')
-    .get('.nav-button-container').contains("Favorites")
-    .get('.nav-button-container').contains("Logout")
-    .get('.guest-button').contains("Continue as Guest")
-    .get('.guest-button').contains('Continue as Guest').click()
-    .location('pathname').should('eq', '/home')
-    .get('.search-form-container').should('exist')
-    .get('.search-input-city').type('Littleton')
-    .get('.search-input-street').type('6805 S Webster Street')
-    .get('.search-input-state-address').type('Colorado')
-    // cy.intercept("POST", "https://reportcard-rails.herokuapp.com/api/v1/district_data", postStub)
-    .get('.search-button').contains('Search').click()
-
-
-  });
-
+  
   // it('can continue as guest but cannot search for district unless user enters city, street, and state', () => {
   //   cy.get('.nav-bar').contains('ReportCard')
   //   .location('pathname').should('eq', '/')
