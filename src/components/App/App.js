628badd060ec497f572ff68a8d920f51763ee169
Patrick Becker <patrickgbecker@gmail.com>
2022-10-22 16:27:42 -0500

    Add userInfo component and re-routing user

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,43 +7,33 @@ import SearchPage from '../SearchPage/SearchPage';
 import UserLoginPage from '../UserLoginPage/UserLoginPage';
 import FavoriteDistrictsPage from '../FavoriteDistrictsPage/FavoriteDistrictsPage';
 import { Routes, Route, useNavigate } from 'react-router-dom';
-// import { useQuery, gql } from '@apollo/client';
+
 
 const App = () => {
   const navigate = useNavigate()
-  const [userName, setUserName] = useState("")
   const [districtData, setDistrictData] = useState({})
-  // const [userCredentials, setUserCredentials] = useState({})
+  const [userLoginEmail, setUserLoginEmail] = useState("")
+  const [userID, setUserID] = useState("")
 
-  // const { error, loading, data } = useQuery(USER_LOGIN_QUERY)
-  
-  // console.log({error, loading, data})
-  // console.log(data)
-  // console.log(error)
-  // setUserName(data)
-  
-  const submitLogin = (data) => {
-    console.log("DATA", data)
+  const submitLogin = (userEmail) => {
+    setUserLoginEmail(userEmail)
+    }
 
+  const signedInUser = (data) => {
+    setUserID(data.user.id)
+  }
 
-    // data && console.log(data)
-      setUserName(data)
-    
-
-    //the 'userCredentials' needs to then update the USER_LOGIN_QUERY user(argument for id or email or whatver we end up doing) 
-    // then if error invite user to log in as guest
-    // if loading show loading
-    //if data exists navigate to the search page.  show user name in nav bar, show favorites button in nav bar
-    // setUserName(userCredentials)
-}
+  const signedOutUser = () => {
+    setUserID("")
+    setUserLoginEmail("")
+  }
 
   const searchForAddress = (newAddressQuery) => {
     getDistrict(newAddressQuery)
   }
-  
   const getDistrict = (addressObject) => {
-    console.log('ADDRESS OBJECT', addressObject)
-    fetch(`https://reportcard-rails.herokuapp.com/api/v1/district_data`, {
+    // console.log('ADDRESS OBJECT', addressObject)
+    return fetch(`https://reportcard-rails.herokuapp.com/api/v1/district_data`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
@@ -52,50 +42,50 @@ const App = () => {
     })
     .then(response => response.json())
     .then(result => {
-      console.log('RESULT', result)
+      // console.log('RESULT', result)
       setDistrictData(result)
       navigate('/district-info')
     })
-
+    .catch(function (error) {
+      console.log(error);
+    });
   }
-  
+
   return (
     <div className="App">
-      <NavBar  userName={userName}/>
-        
+      <NavBar
+        signedInUser={signedInUser}
+        signedOutUser={signedOutUser}
+        userLoginEmail={userLoginEmail}
+      />
 
       <Routes>
-        <Route exact path='/login' element={
-          <UserLoginPage submitLogin={submitLogin}/>
-        } />
-        
-        <Route path='/' element={
+        <Route exact path='/' element={
           <Overview />
         } />
-  
-        <Route path='/home' element={
+        <Route exact path='/login' element={
+          <UserLoginPage submitLogin={submitLogin} />
+        } />
+        <Route exact path='/home' element={
           <SearchPage searchForAddress={searchForAddress} />
         }
         />
-
-        <Route path='/district-info' element={
+        <Route exact path='/district-info' element={
           <DistrictInfoPage districtData={districtData} />
         } />
-
-        <Route path='/favorite-districts' element={
-          <FavoriteDistrictsPage />
+        <Route exact path='/favorite-districts' element={
+          <FavoriteDistrictsPage
+          userID={userID}
+          />
         } />
-
         <Route path='*' element={
           <div>
             <h1 className='not-found'>404: Not found</h1>
             <button className='search-page' onClick={() => navigate('/home')} >Back to Search Page</button>
           </div>
         } />
-
       </Routes>
     </div>
   );
 }
-
 export default App;
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,27 +1,19 @@
 import React from 'react';
 import './NavBar.scss';
-import {  useNavigate } from 'react-router-dom';
+import UserInfo from '../UserInfo/UserInfo'
 
-
-const NavBar = ({username}) => {
-    const navigate = useNavigate()
-    // a note about the buttons.  The buttons for routing home, favorites, etc will need to be conditionally rendered by the path of the url.  
-    // Example: when on the favorites page we dont want a button that routes us to the favorites page since we are already on it
-    // only want to show logout button if user is logged in.  Should be a "create account" button if they continue as guest
-    return (
-        <div className='nav-bar'>
-            <div className='logo-and-buttons'>
-                <div className='logo'>
-                    ReportCard ✅ 
-                    <h1>{username}</h1>
+const NavBar = ({ userLoginEmail, signedOutUser }) => {
+    console.log('hello')
+        return (
+            <div className='nav-bar'>
+                <div className='logo-and-buttons'>
+                    <div className='logo'>
+                        ReportCard ✅
+                    </div>
+                    <UserInfo userLoginEmail={userLoginEmail} signedOutUser={signedOutUser} />
                 </div>
-            <div className='nav-button-container'>
-                <button className='go-to-favorites-page' onClick={ () => navigate('/favorite-districts') }>Favorites</button>
-                <button className='return-to-login-page-button' onClick={ () => navigate('/') }>Logout</button> 
             </div>
-            </div>
-        </div>
-    )
-}
+        )
+    }
 
 export default NavBar;
