8e3ec5a9c9a3d58c161727b925ba921682103efc
Eddie Rodriguez <edwardjrodriguez17@gmail.com>
2022-10-24 11:41:12 -0600

    Update gql mutation to get correct response

--- src/hooks/useGetFavorites.js
+++ src/hooks/useGetFavorites.js
@@ -1,18 +1,22 @@
 import { useQuery, gql } from '@apollo/client';
 
 const USER_FAV_QUERY = gql`
-query userdistricts($userId: Int!){  
-    userdistricts(userId: $userId){
-        id
-        district {
-            name
-            urbanCentricLocale
-            enrollment
+    query userdistricts($userId: Int!){  
+        userdistricts(userId: $userId){
+            id
+            district {
+                name
+                studentTeacherRatio
+                instructionSalaryPercentOfTotal
+                perTeacherSalaryExpenses
+                enrollment
+                numberOfSchoolsInDistrict
+                studentGuidanceCounselorRatio
+                perStudentExpenditure
+            }
         }
     }
-  }
 `;
-
 
 export const useGetFavorites = (userId) => {
     const { error: favError, loading: favLoading, data: favData } = useQuery(USER_FAV_QUERY, {
