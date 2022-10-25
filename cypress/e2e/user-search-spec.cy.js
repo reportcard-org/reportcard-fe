7a7305c44e1ff2831bf479748437b2c9861d044f
Patrick Becker <patrickgbecker@gmail.com>
2022-10-25 14:41:25 -0500

    refactor

--- cypress/e2e/user-search-spec.cy.js
+++ cypress/e2e/user-search-spec.cy.js
@@ -92,7 +92,7 @@ describe('ReportCard search page', () => {
       .get('.search-button').contains('Search').click()
       .url().should('eq', 'http://localhost:3000/district-info')
 
-      .get('.App > .district-info-container > .report-card > .card-bubble').first().contains('Student to teacher')
+      .get('.district-info-container > .report-card > .card-bubble').first().contains('Student to teacher')
       .get('.card-bubble').eq(1).contains(59.39)
       .get('.card-bubble').eq(2).contains('Teacher salary')
       .get('.card-bubble').eq(3).contains('Enrollment')
