8f0e9a80b5fe8fcbc0c75a20af645591713e9d9b
Maia Cochran <maia.cochran1@gmail.com>
2022-10-25 14:48:08 -0700

    Refactor navbar for logo to route to /home

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,48 +2,48 @@ import React from 'react';
 import './NavBar.scss';
 import { useNavigate } from 'react-router-dom';
 
-const NavBar = ({ signOut, data}) => {
+const NavBar = ({ signOut, data }) => {
     const navigate = useNavigate()
-    
+
     if (data) {
-        return(
+        return (
             <div className='nav-bar'>
                 <div className='logo-and-buttons'>
-                    <button onClick={ () => navigate('/') }className='logo'>
+                    <button onClick={() => navigate('/home')} className='logo'>
                         ReportCard ✅
-                </button>
-            <div className='nav-button-container'>
-                <button className='go-to-favorites-page' onClick={ () => navigate('/favorite-districts')}>Favorites</button>
-                <button className='return-to-login-page-button' onClick={ () => signOut() }>Logout</button>
+                    </button>
+                    <div className='nav-button-container'>
+                        <button className='go-to-favorites-page' onClick={() => navigate('/favorite-districts')}>Favorites</button>
+                        <button className='return-to-login-page-button' onClick={() => signOut()}>Logout</button>
                         <h3>{data.user.name}</h3>
-            </div>
+                    </div>
                 </div>
             </div>
         )
-    } else if (!data){
-            return(
-                <div className='nav-bar'>
+    } else if (!data) {
+        return (
+            <div className='nav-bar'>
                 <div className='logo-and-buttons'>
-                    <button onClick={ () => navigate('/') } className='logo'>
+                    <button onClick={() => navigate('/home')} className='logo'>
                         ReportCard ✅
-                </button>
+                    </button>
                     <div className='nav-button-container'>
-                    <button className='return-to-login-page-button' onClick={ () => navigate('/login') }>Sign in</button>
+                        <button className='return-to-login-page-button' onClick={() => navigate('/login')}>Sign in</button>
                     </div>
                 </div>
             </div>
-            )
-        } else {
-            return(
-                <div className='nav-bar'>
-                    <div className='logo-and-buttons'>
-                        <button onClick={ () => navigate('/') }className='logo'>
-                            ReportCard ✅
-                         </button>
-                    </div>
+        )
+    } else {
+        return (
+            <div className='nav-bar'>
+                <div className='logo-and-buttons'>
+                    <button onClick={() => navigate('/home')} className='logo'>
+                        ReportCard ✅
+                    </button>
                 </div>
-            )
-        }
+            </div>
+        )
+    }
 }
 
 export default NavBar;
