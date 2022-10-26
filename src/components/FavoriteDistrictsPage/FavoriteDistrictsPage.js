37d0cd6575a6566831e59c5cf24bf170bbc8fcdb
Eddie Rodriguez <edwardjrodriguez17@gmail.com>
2022-10-26 07:29:27 -0600

    Fix compile error

--- src/components/FavoriteDistrictsPage/FavoriteDistrictsPage.js
+++ src/components/FavoriteDistrictsPage/FavoriteDistrictsPage.js
@@ -2,11 +2,9 @@ import React from 'react';
 import './FavoriteDistrictsPage.scss';
 // import PropTypes from 'prop-types'
 import FavoriteReportCard from '../FavoritedReportCard/FavoriteReportCard';
-import { useNavigate } from 'react-router-dom';
 import { v4 as uuidV4 } from "uuid"
 
 const FavoriteDistrictsPage = ({ favData }) => {
-    const navigate = useNavigate()
 
     const favReportCard = favData.userdistricts.map(district => {
         return (
